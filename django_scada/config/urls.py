f93d9a47d73ec6ac84a41fe62ab61a46abd7c5b7
Oleg Ushakov <olegushakov38@gmail.com>
2026-09-07 20:50:46 +0200

    fix

--- django_scada/config/urls.py
+++ django_scada/config/urls.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
-urlpatterns=[path('admin/', admin.site.urls), path('', include('scada.urls'))]
+from django.views.generic import RedirectView
+
+urlpatterns=[
+    path('', RedirectView.as_view(url='/admin/', permanent=False)),
+    path('admin/', admin.site.urls),
+    path('', include('scada.urls')),
+]
